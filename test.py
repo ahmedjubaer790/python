486e402f54f239802f516d9491f7cedc641cd6b1
ahmedjubaer790 <ahmedjubaer790@gmail.com>
2025-09-25 00:36:30 +0600

    shows how to remove unnencerry spaces

---
+++ test.py
@@ -0,0 +1,8 @@
+msg="Bangladesh "
+print(msg.rstrip()) #right side space remove
+
+msg2="  Bangladesh"
+print(msg2.lstrip()) #left side space remove
+
+msg3="  Bangladesh  "
+print(msg3.strip()) #both side space remove
