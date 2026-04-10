61d6ae69f73af0821fd531caf0de735a68a7e4ca
ahmedjubaer790 <ahmedjubaer790@gmail.com>
2026-04-11 02:58:52 +0600

    Loop list

---
+++ lp_list.py
@@ -0,0 +1,10 @@
+a = ["apple", "banana", "orange"]
+for x in a:
+    print(x)
+#loop with index
+for i in range(len(a)):
+    print('index:', i, 'value:', a[i])
+#reverse loop
+for item in reversed(a):
+    print(item)
+    
