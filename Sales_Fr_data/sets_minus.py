4f5ad2dacae1ce6e4e36d9848e3c49f37f5d53a1
ahmedjubaer790 <ahmedjubaer790@gmail.com>
2026-04-21 10:50:06 +0600

    Python prct

---
+++ Sales_Fr_data/sets_minus.py
@@ -0,0 +1,5 @@
+basket1={"apple","banana","cherry"}
+basket2={"banana","cherry","date"}
+
+print(basket1 - basket2)  # Items in basket1 but not in basket2
+print(basket2 - basket1)  # Items in basket2 but not in basket1
